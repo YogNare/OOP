2b0017fd1372b7a2d2b2e241edaccc0511329e9b
Mikle <mikle@MacBook-Air-Tanya.local>
2024-10-14 13:26:53 +0700

    a

---
+++ Task_1_1_1/src/main/java/ru/nsu/baev/Main.java
@@ -0,0 +1,82 @@
+package ru.nsu.baev;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void heapify(int[] arr, int n, int i) {
+
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest]) largest = left;
+        if (right < n && arr[right] > arr[largest]) largest = right;
+
+        if (largest != i) {
+            swap(arr, largest, i);
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
+    public static int heap_sort(int[] arr) {
+
+        for (int i = arr.length - 1; i >= 0; i --) {
+
+            heapify(arr, arr.length, i);
+        }
+
+        for (int i = arr.length - 1; i >= 0; i --) {
+            swap(arr, i, 0);
+            heapify(arr, i, 0);
+        }
+
+        return 0;
+    }
+
+
+    public static void print_arr(int[] arr) {
+
+        for (int i = 0; i < arr.length; i ++) {
+
+            System.out.print(arr[i]);
+        }
+
+        System.out.println("");
+    }
+
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            while (scanner.hasNextInt()) {
+                int number = scanner.nextInt();
+                list.add(number);
+            }
+        }
+
+        int[] numbers = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            numbers[i] = list.get(i);
+        }
+
+        heap_sort(numbers);
+
+        print_arr(numbers);
+    }
+}
---
+++ Task_1_1_1/src/test/java/ru/nsu/baev/MainTest.java
@@ -0,0 +1,25 @@
+package ru.nsu.baev;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void testGetGreeting() {
+        Main main = new Main();
+        String greeting = main.getGreeting();
+        assertEquals("Hello World!", greeting, "Greeting should be 'Hello World!'");
+    }
+
+    @Test
+    void testHeapSort() {
+        Main main = new Main();
+        int[] input = {3, 1, 4, 1, 5, 9, 2};
+        int[] expected = {1, 1, 2, 3, 4, 5, 9};
+
+        Main.heap_sort(input);
+
+        assertArrayEquals(expected, input, "Array should be sorted");
+    }
+}
