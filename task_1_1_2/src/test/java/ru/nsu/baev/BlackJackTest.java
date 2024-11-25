9f86af1123d2d9e87f415f4e4688a16d0833ece9
Mikle <mikle@MacBook-Air-Chtulhu.local>
2024-11-25 13:27:51 +0700

    New commit

--- task_1_1_2/src/test/java/ru/nsu/baev/BlackJackTest.java
+++ task_1_1_2/src/test/java/ru/nsu/baev/BlackJackTest.java
@@ -1,13 +1,15 @@
 package ru.nsu.baev;
 
+
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import org.junit.jupiter.api.Test;
+
 
 class BlackJackTest {
 
     @Test
     public void whenAssertingArraysEquality() {
-        char[] expected = {'J','u','n','i','t'};
+        char[] expected = {'J', 'u', 'n', 'i', 't'};
         char[] actual = "Junit".toCharArray();
 
         assertArrayEquals(expected, actual);
