0fdd62f6835abc3d79d1d27234daa4700829c898
Mikle <mikle@MacBook-Air-Chtulhu.local>
2024-11-25 13:23:11 +0700

    Hope

--- task_1_1_2/src/test/java/ru/nsu/baev/BlackJackTest.java
+++ task_1_1_2/src/test/java/ru/nsu/baev/BlackJackTest.java
@@ -1,7 +1,7 @@
 package ru.nsu.baev;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 class BlackJackTest {
 
