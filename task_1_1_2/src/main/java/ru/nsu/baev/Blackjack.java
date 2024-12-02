13e3e0b63d1e443a7e0e5909c719d83e7898abdb
Черемушкин Евгений Сергеевич <e.cheremushkin@main.nsu.ru>
2024-12-02 13:15:16 +0700

    refactoring of blackjack class

--- task_1_1_2/src/main/java/ru/nsu/baev/Blackjack.java
+++ task_1_1_2/src/main/java/ru/nsu/baev/Blackjack.java
@@ -5,26 +5,34 @@ import java.util.Scanner;
 /** blackjack class */
 public class Blackjack {
 
-  private final Scanner scanner = new Scanner(System.in);
   private Deck deck = new Deck();
   private Dealer dealer = new Dealer(deck);
   private Player player = new Player(deck);
 
-  //    private int checkResult() {
-  //
-  //        if (this.player.sum > 21) {
-  //            return 2;
-  //        }
-  //        if (this.player.sum)
-  //
-  //        return 0;
-  //    }
+  public static void main(String[] args) {
+    Blackjack black_game = new Blackjack();
+    black_game.game();
+  }
 
-  /**
-   * init dealer. init player.
-   *
-   * @return 3 if both blackjack, 2 if dealer blackjack, 1 if player blackjack else 0.
-   */
+  public void game() {
+    boolean end_game = false;
+
+    int round = 1;
+
+    Scanner scanner = new Scanner(System.in);
+
+    while (!end_game) {
+      System.out.println("Do you want play round " + round++);
+      String choice = scanner.nextLine();
+
+      if (choice.equals("no")) {
+        end_game = true;
+      } else {
+        playRound(scanner);
+      }
+    }
+  }
+
   private int initRound() {
     int dealer_blackjack = this.dealer.startRound();
     int player_blackjack = this.player.startRound();
@@ -46,11 +54,41 @@ public class Blackjack {
     return 0;
   }
 
-  /** print score. */
-  private void printScore() {
-    System.out.println("player " + this.player.win + "/" + this.dealer.win + " dealer");
-  }
+  private void playRound(Scanner scanner) {
+    int result = 0;
 
+    int blackjackRes = initRound();
+    if (blackjackRes > 0) {
+      printResult(result, blackjackRes);
+      return;
+    }
+
+    result = this.player.playerTurn(scanner, dealer);
+    if (result == 1) {
+      this.dealer.win++;
+      printResult(2, 0);
+      return;
+    }
+
+    result = this.dealer.dealerTurn(scanner, player);
+    if (result == 1) {
+      this.player.win++;
+      printResult(1, 0);
+      return;
+    }
+
+    if (this.dealer.sum > this.player.sum) {
+      this.dealer.win++;
+      printResult(2, 0);
+    } else if (this.dealer.sum == this.player.sum) {
+      this.player.win++;
+      this.dealer.win++;
+      printResult(0, 0);
+    } else {
+      this.player.win++;
+      printResult(1, 0);
+    }
+  }
   /**
    * print result.
    *
@@ -79,68 +117,9 @@ public class Blackjack {
 
     printScore();
   }
-
-  /** game func. */
-  public void game() {
-
-    boolean end_game = false;
-
-    String choice = "";
-    int result;
-    int blackjack_res;
-    int round = 1;
-
-    while (!end_game) {
-      System.out.println("Do you want play round " + round++);
-      choice = this.scanner.nextLine();
-      if (choice.equals("no")) {
-        return;
-      }
-
-      result = 0;
-
-      blackjack_res = initRound();
-      if (blackjack_res > 0) {
-        printResult(result, blackjack_res);
-        continue;
-      }
-
-      result = this.player.playerTurn(scanner, dealer);
-      if (result == 1) {
-        this.dealer.win++;
-        printResult(2, 0);
-        continue;
-      }
-
-      result = this.dealer.dealerTurn(scanner, player);
-      if (result == 1) {
-        this.player.win++;
-        printResult(1, 0);
-        continue;
-      }
-
-      if (this.dealer.sum > this.player.sum) {
-        this.dealer.win++;
-        printResult(2, 0);
-      } else if (this.dealer.sum == this.player.sum) {
-        this.player.win++;
-        this.dealer.win++;
-        printResult(0, 0);
-      } else {
-        this.player.win++;
-        printResult(1, 0);
-      }
-    }
+  /** print score. */
+  private void printScore() {
+    System.out.println("player " + this.player.win + "/" + this.dealer.win + " dealer");
   }
 
-  /**
-   * main.
-   *
-   * @param args - args for main.
-   */
-  public static void main(String[] args) {
-    Blackjack black_game = new Blackjack();
-
-    black_game.game();
-  }
 }
