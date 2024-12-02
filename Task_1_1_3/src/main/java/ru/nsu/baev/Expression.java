b151660a00ca4286f068a52f7bd33635941857c1
mikle <mysticnare@gmail.com>
2024-12-02 19:25:38 +0700

    deleted:    Task_1_1_3/.gitignore 	refactored:   Task_1_1_3/src/main/java/ru/nsu/baev/Expression.java 	refactored:   Task_1_1_3/src/test/java/ru/nsu/baev/TestExpression.java

--- Task_1_1_3/src/main/java/ru/nsu/baev/Expression.java
+++ Task_1_1_3/src/main/java/ru/nsu/baev/Expression.java
@@ -1,6 +1,6 @@
 package ru.nsu.baev;
 
-import java.util.Map;
+import java.util.*;
 
 public abstract class Expression {
 
@@ -10,40 +10,85 @@ public abstract class Expression {
 
     public abstract double normal_eval(Map<String, Double> variables);
 
-    private static Expression take_expression(String expr, int priority) {
-        char symbol;
-        int num = 0;
-        Expression left = null;
-        int negative = 1;
+    private static List<String> tokenize(String expression) {
+        List<String> tokens = new ArrayList<>();
+        StringBuilder number = new StringBuilder();
 
-        for (int i = 0; i < expr.length(); i++) {
-            symbol = expr.charAt(i);
-
-            if (symbol == '-' && i + 1 < expr.length() && Character.isDigit(expr.charAt(i + 1))) {
-                negative = -1;
-                continue;
-            }
-
-            if (Character.isDigit(symbol)) {
-                num = num * 10 + (symbol - '0');
+        for (char ch : expression.toCharArray()) {
+            if (Character.isDigit(ch) || (ch == '-' && number.isEmpty())) {
+                number.append(ch);
             } else {
-                if (left == null) {
-                    num *= negative;
-                    left = new Number(num);
-                    num = 0;
+                if (!number.isEmpty()) {
+                    tokens.add(number.toString());
+                    number.setLength(0);
                 }
-
-                if (symbol == '+') {
-                    return new Add(left, take_expression(expr.substring(i + 1), priority));
+                if (!Character.isWhitespace(ch)) {
+                    tokens.add(Character.toString(ch));
                 }
             }
         }
+        if (!number.isEmpty()) {
+            tokens.add(number.toString());
+        }
+        return tokens;
+    }
 
-        if (left == null) {
-            num *= negative;
-            left = new Number(num);
+    private static int priority(String operator) {
+        return switch (operator) {
+            case "+", "-" -> 1;
+            case "*", "/" -> 2;
+            default -> 0;
+        };
+    }
+
+    private static void applyOperator(Stack<Expression> expressions, String operator) {
+        Expression right = expressions.pop();
+        Expression left = expressions.pop();
+        Expression result = switch (operator) {
+            case "+" -> new Add(left, right);
+            case "-" -> new Sub(left, right);
+            case "*" -> new Mul(left, right);
+            case "/" -> new Div(left, right);
+            default -> throw new IllegalArgumentException("Unknown operator: " + operator);
+        };
+        expressions.push(result);
+    }
+
+    public static Expression evaluate(String expression) {
+        List<String> tokens = tokenize(expression);
+        Stack<Expression> expressions = new Stack<>();
+        Stack<String> operators = new Stack<>();
+
+        for (String token : tokens) {
+            if (token.matches("-?\\d+")) {
+                expressions.push(new Number(Integer.parseInt(token)));
+            } else if (token.matches("[a-zA-Z]+")) {
+                expressions.push(new Variable(token));
+            } else if (token.equals("(")) {
+                operators.push(token);
+            } else if (token.equals(")")) {
+                while (!operators.isEmpty() && !operators.peek().equals("(")) {
+                    applyOperator(expressions, operators.pop());
+                }
+                if (!operators.isEmpty() && operators.peek().equals("(")) {
+                    operators.pop();
+                }
+            } else {
+                while (!operators.isEmpty() && priority(operators.peek()) >= priority(token)) {
+                    applyOperator(expressions, operators.pop());
+                }
+                operators.push(token);
+            }
         }
 
-        return left;
+        while (!operators.isEmpty()) {
+            applyOperator(expressions, operators.pop());
+        }
+
+        if (expressions.size() != 1) {
+            throw new IllegalArgumentException("Invalid expression");
+        }
+
+        return expressions.pop();
     }
 }
--- Task_1_1_3/src/test/java/ru/nsu/baev/TestExpression.java
+++ Task_1_1_3/src/test/java/ru/nsu/baev/TestExpression.java
@@ -1,4 +1,23 @@
 package ru.nsu.baev;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static ru.nsu.baev.Expression.evaluate;
+
+import org.junit.jupiter.api.Test;
+
 public class TestExpression {
+
+    @Test
+    public void printTest() {
+        Expression e = new Add(new Number(3), new Mul(new Number(2),
+                new Variable("x"))); // (3+(2*x))
+        e.print();
+    }
+
+    @Test
+    public void test1() {
+
+        Expression e = evaluate("(3+(-2*x))");
+        e.print();
+    }
 }
